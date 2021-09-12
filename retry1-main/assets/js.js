26a3bc29f0c5dba8aa85d69c5fa86fcc240e1c0d
EzzEldinMahmoud <72217061+EzzEldinMahmoud@users.noreply.github.com>
2021-09-12 23:01:05 +0200

    Add files via upload

---
+++ retry1-main/assets/js.js
@@ -0,0 +1,15 @@
+  
+window.addEventListener("load", function(){
+    feather.replace();
+    
+});
+
+$(document).ready(function() {
+    $(".hide").click(function() {
+      $(".menu").hide();
+    });
+    $(".show").click(function() {
+      $(".menu").show();
+    });
+  });
+  
